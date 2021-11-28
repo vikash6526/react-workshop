2ae369534eb440dea67ae66ef96a3fe7bb74d2c0
vikash <vikashkumar1003@gmail.com>
2021-11-28 12:38:06 -0500

    Added User input form

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,9 +1,11 @@
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 const App = () => {
   return (
     <div>
       <h2>Expenses!</h2>
+      <NewExpense />
       <Expenses />
     </div>
   );
